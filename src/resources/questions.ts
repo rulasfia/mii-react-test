4eb54a1fdb380d8d38761afe9157dda66d6d42c4
Khoirul Asfian <mk.asfian@gmail.com>
2023-07-26 10:10:55 +0700

    feat: add list of questions + setup prettier

---
+++ src/resources/questions.ts
@@ -0,0 +1,56 @@
+export const questions = [
+	{
+		id: "6cf10864-af12-4b89-855b-c387f7f16ace",
+		question: "How often do you exercise?",
+		answers: ["Daily", "3-4 times a week", "Rarely or never"],
+	},
+	{
+		id: "56ed7455-a7ea-4638-ae63-82d1726df8bc",
+		question: "Which type of cuisine do you prefer the most?",
+		answers: ["Italian", "Chinese", "Other"],
+	},
+	{
+		id: "62805bc8-8c9b-4966-9cac-368d68e71e8e",
+		question: "What age group do you belong to?",
+		answers: ["18-24", "25-34", "35 and above"],
+	},
+	{
+		id: "c3a11854-3afc-4574-8026-04e30b47e487",
+		question: "Which social media platform do you use most frequently?",
+		answers: ["Facebook", "Instagram", "Other"],
+	},
+	{
+		id: "017f2fe6-8b77-4800-8793-4961d6d78327",
+		question: "How often do you shop online?",
+		answers: ["Weekly", "Monthly", "Rarely or never"],
+	},
+	{
+		id: "11827958-5674-4ae8-83a5-55fc1990d3b0",
+		question: "Which genre of movies do you enjoy the most?",
+		answers: ["Action/Adventure", "Comedy", "Other"],
+	},
+	{
+		id: "0e1d3cfe-013a-461a-912e-57288b24f2d7",
+		question:
+			"How satisfied are you with the customer service provided by our company?",
+		answers: ["Very satisfied", "Satisfied", "Dissatisfied"],
+	},
+	{
+		id: "87bab8ba-a2b9-45d9-950f-6aa8921465ce",
+		question:
+			"What is your preferred mode of transportation for daily commuting?",
+		answers: ["Car", "Public transportation (bus/train)", "Other"],
+	},
+	{
+		id: "61c509ed-0794-4e21-ae0f-bd7f5ec67013",
+		question:
+			"How likely are you to recommend our product/service to a friend or colleague?",
+		answers: ["Very likely", "Likely", "Unlikely"],
+	},
+	{
+		id: "4ee52386-5818-4861-a574-0bfeaa9f3050",
+		question:
+			"Which of the following factors influences your purchasing decisions the most?",
+		answers: ["Price", "Product quality", "Other"],
+	},
+];
